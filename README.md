0940025b290e0d3f38cee9286db8e59d9b4de239
Aapka Naam <aapka_email@example.com>
2024-05-22 17:20:23 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# h1d5-calculator-full" 
---
+++ main.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+
+const answer = await inquirer.prompt([
+    {message:"enter your first number", type:"number", name:"firstnumber"},
+    {message:"enter your second number", type:"number",name:"secondnumber"},
+    {message:"select one of the oprator to perform opration",
+        type:"list",
+        name:"oprator",
+        choices:["addition","substract","multiplication","division"]
+    }
+]);
+
+
+// conditional statements
+
+if (answer.oprator==="addition"){
+    console.log(answer.firstnumber+answer.secondnumber);
+}else if (answer.oprator==="substract"){
+    console.log(answer.firstnumber-answer.secondnumber);
+}else if(answer.oprator==="multiplication"){
+    console.log(answer.firstnumber*answer.secondnumber);
+}else if(answer.oprator==="division"){
+    console.log(answer.firstnumber/answer.secondnumber);
+}else{
+    console.log("select the valid oprator")
+}
+
+
+
+
+
+
+// packages
+// tsc --init
+// npm init -y
+// npm i @types/node -D     // npm i inquirer
+// npm i --save-dev @types/inquirer
+// .gitignore
+// git clone link....     // from github folder to vs code
+// tsc main.ts & node main.js
+// cls // for clean terminal
