23e220fa87bfd853658e28a0b8cf79697528e2cd
Sainbux-HadiSeelro <sainbuxseelro11@gmail.com>
2024-05-23 13:48:55 +0500

    "mdssss"

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ import inquirer from "inquirer";
 const answer = await inquirer.prompt([
     {message:"enter your first number:", type:"number", name:"firstnumber"},
     {message:"enter your second number:", type:"number",name:"secondnumber"},
-    {message:"select one of the oprator to perform opration:",
+    {message:"select one of the oprator to perform opration::",
         type:"list",
         name:"oprator",
         choices:["addition","substract","multiplication","division"]
